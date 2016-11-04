2adb3c4500f2ff9c3190f30566e6317831e7022a
Brendan Dugan <brendan@workspan.com>
2016-11-04 14:02:27 -0700

    add framework for api keys

---
+++ app/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "TODO",
+  authDomain: "TODO",
+  databaseURL: "TODO",
+  storageBucket: "TODO",
+  messagingSenderId: "TODO"
+};
+firebase.initializeApp(config);
